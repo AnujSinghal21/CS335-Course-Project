24bfff08ac48b114ec6efaab1758f8ceaf52cb20
= <singhalanuj7575@gmail.com>
2024-04-17 03:22:40 +0530

    Merging conflicts

--- milestone3/out/test.py
+++ milestone3/out/test.py
@@ -1,16 +1,9 @@
-class A():
-
-    def __init__(self,new:int):
-        self.x:int = new
 
 def main() -> None :
     x:int = 4
     y:int = 5
     z: int = x + y
-    a:A = A(5)
-    a.x = 10  
     return
-
 
 if __name__ == "__main__":
     main()
--- milestone3/out/x86.s
+++ milestone3/out/x86.s
@@ -1,15 +1,6 @@
-	.text
-	beginfunc A.__init__@A@int
-	#t_0 = popparam
-	#t_1 = popparam
-	#t_2 = class_get(A, x)
-	#t_3 = NOT_ASSIGNED + 
-	*(NOT ASSIGNED) = #t_0
-	endfunc 
-	beginfunc global.main
-	x = 4
-	y = 5
-	#t_5 = x + y
-	z = #t_5
-	return 
-	endfunc 
+.data
+.text
+	movq $4, %rbx
+	movq %rbx, -8(%rbp)
+	movq $5, %rbx
+	movq %rbx, -16(%rbp)
